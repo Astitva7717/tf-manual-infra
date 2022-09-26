ae9276903febc2858400c6518cf15d295e0c7d8a
Astitva Aggarwal <73715253+Astitva7717@users.noreply.github.com>
2022-09-26 17:29:47 +0530

    Delete g directory

--- g/ec2/main.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_instance" "ubuntu" {
-  ami           = "ami-062df10d14676e201"
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "ubuntu"
-    SpotInstanceAMIBackup = "yes"
-  }
-}
